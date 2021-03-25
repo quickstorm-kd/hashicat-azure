bd3e23ea1f7fe8952b0a86bfe50908fa15137036
kim desormeaux <kimdesormeaux@gmail.com>
2021-03-25 16:02:55 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -123,8 +123,6 @@ resource "azurerm_virtual_machine" "catapp" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  tags = {}
 }
 
 # We're using a little trick here so we can run the provisioner without
